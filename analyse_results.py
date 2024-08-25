e1f349ae826c5f319546cf99be0a87d586a90006
Lumin <lwg342@gmail.com>
2024-08-25 16:45:23 +0100

    save

--- analyse_results.py
+++ analyse_results.py
@@ -1,12 +1,24 @@
 # %%
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from utils.empirical import visualize_prediction
 
-# filename= "UKDA_9248_results_2024-08-14"
-filename = "asec23pub_results_20240819"
-results = pd.read_csv(f"{filename}.csv").drop_duplicates(
+dataset = "LFS"
+
+if dataset == "LFS":  # 🇬🇧 UKDA_9248 data
+    results = pd.read_csv("UKDA_9248_results_2024-08-23.csv")
+    data = pd.read_csv("wage-data/clean_apsp_jd23_eul_pwta22.tab")
+if dataset == "CPS":  # 🇺🇸 US CPS data
+    results = pd.read_csv("asec23pub_results_20240819.csv")
+    data = pd.read_csv("wage-data/clean_data_asec_pppub23.csv")
+
+results = results.drop_duplicates(
     subset=["Education", "Alpha", "Experience"], keep="first"
 )
+
+df_holdout = data[data["Is_holdout"]]
+
 results
 # %%
 columns_to_include = [
@@ -38,6 +50,7 @@ for col in [
 
 
 # %%
+# [-] Create table of results
 import pandas as pd
 
 # Create a list to store the new rows
@@ -89,88 +102,116 @@ results_formatted = pd.DataFrame(rows).pivot(
 latex_table = results_formatted.to_latex(
     multicolumn=True,
     multirow=True,
-    label=f"tab:{filename}",
+    label=f"tab:{dataset}",
     longtable=True,
     caption="Conformal prediction and prediction intervals for annual income, by education level and the speficied alpha level. In the method column, P stands for prediction and CP stands for conformal prediction. W indicates prediction without interval censored data. ",
 )
 
-# %%
 print(latex_table)
 # %%
-# [-] Plots
-import pandas as pd
+# [-] Analyse coverage on the holdout data
 
-transform = np.array
 
+def compute_coverage(row):
+    selection = (
+        (df_holdout["Education"] == row["Education"])
+        & (df_holdout["Experience"] >= row["Experience"] - 3)
+        & (df_holdout["Experience"] <= row["Experience"] + 3)
+    )
+    if_exact = df_holdout["BANDG"] < 0
+
+    df_temp = df_holdout[selection]
+    df_temp_only_exact = df_holdout[selection & if_exact]
+
+    # [-] coverage of both exact numbers and intervals
+    lower_cov = df_temp["Lower_bound"] >= float(row["Conformal Prediction Lower Bound"])
+    upper_cov = df_temp["Upper_bound"] <= float(row["Conformal Prediction Upper Bound"])
+
+    lower_cov_with_exact = df_temp["Lower_bound"] >= float(
+        row["Conformal Prediction Lower Bound with Exact Number"]
+    )
+    upper_cov_with_exact = df_temp["Upper_bound"] <= float(
+        row["Conformal Prediction Upper Bound with Exact Number"]
+    )
+
+    # [-] coverage of exactly reported
+    lower_cov_of_exact = df_temp_only_exact["Lower_bound"] >= float(
+        row["Conformal Prediction Lower Bound"]
+    )
+    upper_cov_of_exact = df_temp_only_exact["Upper_bound"] <= float(
+        row["Conformal Prediction Upper Bound"]
+    )
+
+    lower_cov_with_exact_of_exact = df_temp_only_exact["Lower_bound"] >= float(
+        row["Conformal Prediction Lower Bound with Exact Number"]
+    )
+    upper_cov_with_exact_of_exact = df_temp_only_exact["Upper_bound"] <= float(
+        row["Conformal Prediction Upper Bound with Exact Number"]
+    )
+
+    total_num = df_temp.shape[0]
+    total_num_of_exact = df_temp_only_exact.shape[0]
+    if total_num > 0:
+        row["Coverage_conformal"] = sum(lower_cov & upper_cov) / total_num
+        row["Coverage_conformal_with_exact_number"] = (
+            sum(lower_cov_with_exact & upper_cov_with_exact) / total_num
+        )
+
+        row["Coverage_conformal_of_exact"] = (
+            sum(lower_cov_of_exact & upper_cov_of_exact) / total_num_of_exact
+        )
+        row["Coverage_conformal_with_exact_number_of_exact"] = (
+            sum(lower_cov_with_exact_of_exact & upper_cov_with_exact_of_exact)
+            / total_num_of_exact
+        )
+
+    else:
+        row["Coverage_conformal"] = 0
+        row["Coverage_conformal_with_exact_number"] = 0
+
+    return row
+
+
+# Applying the function to each row
+result_with_cov = results.apply(compute_coverage, axis=1)
+# %%
+exp = 10
+alpha = 0.5
+
+results_with_cov_plot = result_with_cov.loc[
+    (result_with_cov["Experience"] == exp) & (result_with_cov["Alpha"] == alpha),
+].sort_values(by="Education")
+import matplotlib.pyplot as plt
 
 plt.figure()
 
-visualize_prediction(
-    results,
-    "prediction interval",
-    transform,
-    f"Prediction (exact and range)",
-    "tab:blue",
-    offset=-0.3,
-)
-visualize_prediction(
-    results,
-    "conformal prediction interval",
-    transform,
-    f"Conf. prediction (exact and range)",
-    "tab:red",
-    offset=-0.1,
-)
-print(
-    np.exp(results["conformal prediction interval"][0]),
-    np.exp(results["conformal prediction interval"][1]),
+# Plot Coverage_conformal
+
+# Plot Coverage_conformal
+results_with_cov_plot.plot(
+    x="Education",
+    y="Coverage_conformal",
+    kind="line",
+    marker="o",
+    label="Coverage_conformal",
+    ax=plt.gca(),  # Plot on the current axes
 )
 
-visualize_prediction(
-    results_exact,
-    "prediction interval",
-    transform,
-    f"Prediction (exact only)",
-    "tab:orange",
-    marker="s",
-    offset=0.1,
+# Plot Coverage_conformal_with_exact_number
+results_with_cov_plot.plot(
+    x="Education",
+    y="Coverage_conformal_with_exact_number",
+    kind="line",
+    marker="o",
+    label="Coverage_conformal_with_exact_number",
+    ax=plt.gca(),  # Plot on the same axes
 )
-visualize_prediction(
-    results_exact,
-    "conformal prediction interval",
-    transform,
-    f"Conf. prediction (exact only)",
-    "tab:green",
-    offset=0.3,
-    marker="s",
-)
-print(
-    np.exp(results_exact["conformal prediction interval"][0]),
-    np.exp(results_exact["conformal prediction interval"][1]),
-)
+plt.axhline(1 - alpha, color="tab:red", linestyle="--")
+# plt.ylim(0.5, 1)
 plt.xlabel("Education")
-plt.ylabel("Predicted log earnings")
+plt.ylabel("Coverage")
+plt.title("Coverage Metrics by Education")
+plt.legend()
+plt.show()
 
-
-plt.plot(
-    results["edu"],
-    results["kernel regression yl"],
-    label="Mean of lower bound",
-    marker="x",
-)
-plt.plot(
-    results["edu"],
-    results["kernel regression yu"],
-    label="Mean of upper bound",
-    marker="x",
-)
-# plt.legend(loc="center right", bbox_to_anchor=(-0.5, -0.1), ncol=1)
-# Place the legend outside the plot area
-# plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
-
-plt.tight_layout()
-plt.title(
-    f"Conformal Prediction Intervals when Experience is fixed at {results['experience']}"
-)
-cdt = pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")
-plt.savefig(f"empirical_edu_lfs_{alpha}_{cdt}.pdf", bbox_inches="tight")
+# %%
--- empirical_UKDA_9248.py
+++ empirical_UKDA_9248.py
@@ -8,24 +8,23 @@ from sklearn.model_selection import train_test_split
 
 
 current_date = pd.Timestamp.now().strftime("%Y-%m-%d")
-filename = "apsp_jd23_eul_pwta22.tab"
 
-data = pd.read_csv(f"wage-data/clean_{filename}")
-df = data[~data["Is_holdout"]]
-df_holdout = data[data["Is_holdout"]]
+data = pd.read_csv("wage-data/clean_apsp_jd23_eul_pwta22.tab")
+df = data[~data["Is_holdout"]].copy()
+df_holdout = data[data["Is_holdout"]].copy( )
 
 result_file_path = f"UKDA_9248_results_{current_date}.csv"
 df["Log_upper_bound"].describe()
 
 # %%
-random_seed = 19260817
+random_seed = 43
 # alpha = 0.9
 # exp_fixed = 15
 bandwidth = np.array(
     [0.48453814, 2.3811055]
 )  # This is from a cross-validation run before this implementation
-for alpha in np.array([0.1,0.5,0.9]):
-    for exp_fixed in np.array([10,20]):
+for alpha in np.array([0.1, 0.5, 0.9]):
+    for exp_fixed in np.array([10, 20]):
         np.random.seed(random_seed)
         print(f"alpha: {alpha}, exp_fixed: {exp_fixed}")
         dt, results = analyze_and_plot(
@@ -88,8 +87,6 @@ for alpha in np.array([0.1,0.5,0.9]):
             df_results.to_csv(result_file_path, mode="a", header=False, index=False)
 
 
-
-
 # %%
 # [-] Check for why some prediction results are the same
 random_seed = 19260817
@@ -125,7 +122,7 @@ def local_cond(df, edu, exp_l, exp_u):
 
 
 edu = 12
-local_cond = local_cond(df_holdout, edu,6, 14)
+local_cond = local_cond(df_holdout, edu, 6, 14)
 print(sum(local_cond) * 0.9)
 print(
     sum(
@@ -164,3 +161,80 @@ print(
 
 
 # %%
+# [-] Plots
+import pandas as pd
+
+transform = np.array
+
+
+plt.figure()
+
+visualize_prediction(
+    results,
+    "prediction interval",
+    transform,
+    f"Prediction (exact and range)",
+    "tab:blue",
+    offset=-0.3,
+)
+visualize_prediction(
+    results,
+    "conformal prediction interval",
+    transform,
+    f"Conf. prediction (exact and range)",
+    "tab:red",
+    offset=-0.1,
+)
+print(
+    np.exp(results["conformal prediction interval"][0]),
+    np.exp(results["conformal prediction interval"][1]),
+)
+
+visualize_prediction(
+    results_exact,
+    "prediction interval",
+    transform,
+    f"Prediction (exact only)",
+    "tab:orange",
+    marker="s",
+    offset=0.1,
+)
+visualize_prediction(
+    results_exact,
+    "conformal prediction interval",
+    transform,
+    f"Conf. prediction (exact only)",
+    "tab:green",
+    offset=0.3,
+    marker="s",
+)
+print(
+    np.exp(results_exact["conformal prediction interval"][0]),
+    np.exp(results_exact["conformal prediction interval"][1]),
+)
+plt.xlabel("Education")
+plt.ylabel("Predicted log earnings")
+
+
+plt.plot(
+    results["edu"],
+    results["kernel regression yl"],
+    label="Mean of lower bound",
+    marker="x",
+)
+plt.plot(
+    results["edu"],
+    results["kernel regression yu"],
+    label="Mean of upper bound",
+    marker="x",
+)
+# plt.legend(loc="center right", bbox_to_anchor=(-0.5, -0.1), ncol=1)
+# Place the legend outside the plot area
+# plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+
+plt.tight_layout()
+plt.title(
+    f"Conformal Prediction Intervals when Experience is fixed at {results['experience']}"
+)
+cdt = pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")
+plt.savefig(f"empirical_edu_lfs_{alpha}_{cdt}.pdf", bbox_inches="tight")
